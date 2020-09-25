d2420453d321f1f5b4d00a8a6694a8e31c157d27
nawhi <25394965+nawhi@users.noreply.github.com>
2020-09-25 09:54:42 +0100

    remove some unnecessary tsconfig

--- service/src/index.ts
+++ service/src/index.ts
@@ -1,13 +1,13 @@
 /*
- * Uncomment this line, and add an import for Greeting
+ * Uncomment the line of code, and add an import for Greeting
  * using the code intention which comes up automatically.
- */
-// const greeting = new Greeting()
-
-/*
+ *
  * The import will be added with the path:
  * '@samplecorp/shared/greeting'
  *
  * but the file is actually at a different path:
  * '@samplecorp/shared/src/greeting'
  */
+
+// const greeting = new Greeting()
+
